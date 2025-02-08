391154ab7b304d002ba8d6d641c0d9b988c9931a
davidkorff <davidkorff@gmail.com>
2025-02-08 00:56:09 -0500

    documentation add

---
+++ StoredProcs/DK_Submission_Search_WS.sql
@@ -0,0 +1,52 @@
+USE [OriginTest]
+GO
+
+ALTER PROCEDURE [dbo].[DK_Submission_Search_WS]
+    @CustomerName NVARCHAR(100) = NULL,
+    @ControlNo NVARCHAR(50) = NULL
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    -- If Control Number is provided, only search by that
+    IF @ControlNo IS NOT NULL
+    BEGIN
+        SELECT 
+            QuoteGUID,
+            QuoteID,
+            ControlNo,
+            InsuredPolicyName as CustomerName,
+            EndorsementComment,
+            DateCreated
+        FROM dbo.tblQuotes
+        WHERE ControlNo = @ControlNo;
+        RETURN;
+    END
+
+    -- If only Customer Name is provided, search by LIKE
+    IF @CustomerName IS NOT NULL
+    BEGIN
+        SELECT 
+            QuoteGUID,
+            QuoteID,
+            ControlNo,
+            InsuredPolicyName as CustomerName,
+            EndorsementComment,
+            DateCreated
+        FROM dbo.tblQuotes
+        WHERE InsuredPolicyName LIKE '%' + @CustomerName + '%';
+        RETURN;
+    END
+
+    -- If neither parameter is provided, return no results
+    SELECT TOP 0 
+        QuoteGUID,
+        QuoteID,
+        ControlNo,
+        InsuredPolicyName as CustomerName,
+        EndorsementComment,
+        DateCreated
+    FROM dbo.tblQuotes;
+END
+
+--exec DK_Submission_Search_WS @controlno=10000
